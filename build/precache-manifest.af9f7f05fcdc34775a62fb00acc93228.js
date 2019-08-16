c7c5c30d1bfeb2d3104f250e658e84c7097c9c5e
Felipe_olvr <lippe252@gmail.com>
2019-08-16 01:01:04 -0300

    Front-End website

---
+++ build/precache-manifest.af9f7f05fcdc34775a62fb00acc93228.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb49fd65914cff9a28c1d628e0c9fe1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc893d1c54bb19ba14a7",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "e21958a790e54da336a8",
+    "url": "/static/js/2.77cd681c.chunk.js"
+  },
+  {
+    "revision": "fc893d1c54bb19ba14a7",
+    "url": "/static/js/main.2e47206f.chunk.js"
+  },
+  {
+    "revision": "b63c8a70acd204d87d33",
+    "url": "/static/js/runtime~main.c41ce42c.js"
+  }
+]);
